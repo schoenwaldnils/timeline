01162a9ec29403f776ccf1644a5b706706a32131
Nils Schönwald <nils@schoenwald.media>
2017-01-01 05:45:40 +0100

    Make persons a menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('bla');
+import { forEach } from 'lodash';
+
+const headerLink = document.querySelector('.Header-link--persons');
+const personsList = document.querySelector('.Header-subNav');
+// const personsLinks = document.querySelectorAll('.Header-subLink');
+
+headerLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  personsList.classList.toggle('is-active');
+});
+
+// forEach(personsLinks, function(personsLink, key) {
+//   personsLink.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     console.log('click');
+//     const name = personsLink.getAttribute('data-name');
+//     const human = document.querySelector(`.Human--${name}`);
+//     // FIXME: remove class from all humans
+//     // forEach(personsLinks, function(personsLink, key) {
+//     //   console.log('remove');
+//     // });
+//     human.classList.add('is-active');
+//   });
+// });
