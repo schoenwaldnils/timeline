0edfd90d6b306bcbe8b8d18513517b62ae945895
Nils Schönwald <nils@schoen.world>
2020-04-19 01:44:06 +0200

    fix storybook svg loader

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
+const sharedConfig = require('./webpack.sharedConfig.js')
+
 module.exports = {
   exportTrailingSlash: true,
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            icon: true,
-            memo: true,
-            svgoConfig: {
-              plugins: [
-                {
-                  cleanupIDs: false,
-                },
-              ],
-            },
-          },
-        },
-        'url-loader',
-      ],
-    })
-
-    return config
-  },
+  webpack: sharedConfig,
 }
