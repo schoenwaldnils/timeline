57c8c357cc931aae0b718e200e03f00aa2c1a7e7
Nils Schönwald <nils@schoen.world>
2020-04-20 18:20:31 +0200

    ignore storybook files on next export

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@ const sharedConfig = require('./webpack.sharedConfig.js')
 
 module.exports = {
   exportTrailingSlash: true,
-  webpack: sharedConfig,
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.stories\.tsx$/,
+      loader: 'ignore-loader',
+    })
+    return { ...config, ...sharedConfig(config) }
+  },
 }
