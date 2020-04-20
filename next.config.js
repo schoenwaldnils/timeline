03a2bf4b70e3d2b9310dc73b75ee70e36b8ba706
Nils Schönwald <nils@schoen.world>
2020-04-20 18:55:17 +0200

    move storybook addon

--- next.config.js
+++ next.config.js
@@ -2,11 +2,5 @@ const sharedConfig = require('./webpack.sharedConfig.js')
 
 module.exports = {
   exportTrailingSlash: true,
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.stories\.tsx$/,
-      loader: 'ignore-loader',
-    })
-    return { ...config, ...sharedConfig(config) }
-  },
+  webpack: sharedConfig,
 }
