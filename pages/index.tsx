0f553c7de7f30be6a850852aa5b655200fccbbd6
Nils Schönwald <nils@schoen.world>
2020-05-06 19:18:50 +0200

    change redirect method

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import React, { useEffect } from 'react'
 import Head from 'next/head'
-import { useRouter } from 'next/dist/client/router'
 import { getInitialLocale } from '../app/utils/intl/getInitialLocale'
 
 const Index: React.FC = () => {
-  const router = useRouter()
-
   useEffect(() => {
-    router.replace('/[lang]/', `/${getInitialLocale()}/`)
+    window.location.href = `${window.location.origin}/${getInitialLocale()}/`
   })
 
   return (
